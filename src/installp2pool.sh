1c423da7d4c8b50a306cc13a0f2c3696e6379031
nahuhh <50635951+nahuhh@users.noreply.github.com>
2022-03-13 15:57:33 -0400

    p2pool install script
    1.8

---
+++ src/installp2pool.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+wget -O p2pool.tar.gz https://github.com/SChernykh/p2pool/releases/download/v1.8/p2pool-v1.8-linux-aarch64.tar.gz
+mkdir p2pool
+tar --one-top-level=p2pool/ --strip-components=1 --keep-newer-files -xvf p2pool.tar.gz
+echo "
+
+
+			Run p2pool using
+
+
+./p2pool/p2pool --host 127.0.0.1 --wallet YOURWALLETADDRESS --mini
+
+
+"
