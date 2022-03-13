3e2e45fd8f01448567bd562409dbac7a388135bc
nahuhh <50635951+nahuhh@users.noreply.github.com>
2022-03-13 18:55:14 -0400

    Update installp2pool.sh

--- src/installp2pool.sh
+++ src/installp2pool.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+apt install wget
 wget -O p2pool.tar.gz https://github.com/SChernykh/p2pool/releases/download/v1.8/p2pool-v1.8-linux-aarch64.tar.gz
 mkdir p2pool
 tar --one-top-level=p2pool/ --strip-components=1 --keep-newer-files -xvf p2pool.tar.gz
