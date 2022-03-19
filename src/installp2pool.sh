07afacf60cea05d98439d955aed4b39a88116e43
nahuhh <50635951+nahuhh@users.noreply.github.com>
2022-03-19 17:24:18 -0400

    Create start script ./startp2pool

--- src/installp2pool.sh
+++ src/installp2pool.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
-apt install wget
+apt install wget nano -y
 wget -O p2pool.tar.gz https://github.com/SChernykh/p2pool/releases/download/v1.8/p2pool-v1.8-linux-aarch64.tar.gz
 mkdir p2pool
 tar --one-top-level=p2pool/ --strip-components=1 --keep-newer-files -xvf p2pool.tar.gz
-echo "
+touch startp2pool
+echo "#!/bin/bash
+
+# Enter your wallet address below
+
+./p2pool/p2pool --wallet YOURWALLETADDRESS --host 127.0.0.1 --mini --no-randomx" > startp2pool
+chmod +x startp2pool
+nano startp2pool
+echo
+"
 
 
-			Run p2pool using
+			To start p2pool, type:
 
 
-./p2pool/p2pool --host 127.0.0.1 --wallet YOURWALLETADDRESS --mini
+			    ./startp2pool
 
-
+Tip: press tab to autofill  (type "./sta{tab}" etc) 
 "
