125718250836fd09f90baea5e5b07bbdd198db16
nahuhh <50635951+nahuhh@users.noreply.github.com>
2022-03-22 14:00:44 -0400

    Improve

--- src/installp2pool.sh
+++ src/installp2pool.sh
@@ -3,14 +3,26 @@ apt install wget nano -y
 wget -O p2pool.tar.gz https://github.com/SChernykh/p2pool/releases/download/v1.8/p2pool-v1.8-linux-aarch64.tar.gz
 mkdir p2pool
 tar --one-top-level=p2pool/ --strip-components=1 --keep-newer-files -xvf p2pool.tar.gz
-touch startp2pool
-echo "#!/bin/bash
-
-# Enter your wallet address below
-
-./p2pool/p2pool --wallet YOURWALLETADDRESS --host 127.0.0.1 --mini --no-randomx" > startp2pool
+echo 'YourAddress
+# Address must begin with 4
+# END to move cursor to the end
+# ALT backspace to delete YourAddress
+# Tap and hold to paste your actual address
+# To save: Ctrl - x - y - enter' > walletaddress.xmr
+cat << 'EOF' > startp2pool
+#!/bin/bash
+./p2pool/p2pool \
+--host 127.0.0.1 \
+--rpc-port 18081
+--stratum 127.0.0.1:3333 \
+--loglevel 0 \
+--mini \
+--no-randomx \
+--wallet \
+EOF
+nano walletaddress.xmr
+cat < walletaddress.xmr >> startp2pool
 chmod +x startp2pool
-nano startp2pool
 echo
 '
 
@@ -18,7 +30,4 @@ echo
 			To start p2pool, type:
 
 
-			    ./startp2pool
-
-Tip: press tab to autofill  (type "./sta{tab}" etc) 
-'
+			    ./startp2pool'
